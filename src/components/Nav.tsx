7a704d031ae50e16e150de04fea55d308c186f0a
Amanshu Kataria <amanshukataria@hotmail.com>
2020-05-24 15:06:29 +0530

    app menu

---
+++ src/components/Nav.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { Label, Menu } from 'semantic-ui-react';
+
+const menuItems = ['Repositories', 'Stars', 'Followers', 'Following'];
+
+function Nav() {
+  const [activeItem, setActiveItem] = useState(0);
+  return (
+    <Menu vertical className="gg__nav">
+      {menuItems.map((item, index) => {
+        return (
+          <Menu.Item name={item} active={activeItem === index} onClick={() => setActiveItem(index)}>
+            <Label color={activeItem === index ? 'teal' : 'grey'}>1</Label>
+            {item}
+          </Menu.Item>
+        );
+      })}
+    </Menu>
+  );
+}
+
+export default Nav;
