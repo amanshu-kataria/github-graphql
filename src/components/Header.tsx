4680d980106b6da9ef10e93788fd50f8affde868
Amanshu Kataria <amanshukataria@hotmail.com>
2020-05-24 15:04:54 +0530

    app header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useAuth0 } from './AuthContext';
+import { IAuthContext } from '../interfaces/auth';
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="header">
-        <h4>Github + GraphQL</h4>
+function Header() {
+  const { user }: IAuthContext = useAuth0();
+  const profilePicture = `${user.picture}&s=25`;
+
+  return (
+    <div className="gg__header">
+      <div className="gg__app-title">Github + GraphQL</div>
+      <div className="gg__user-section">
+        <span className="gg__user-name">{user.name}</span>
+        <img className="gg__user-picture" src={profilePicture} />
+        <div className="gg__user-dropdown"></div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Header;
