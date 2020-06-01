f814ff89661560f60943b3742be08a956c92eebe
Amanshu Kataria <amanshukataria@hotmail.com>
2020-06-02 00:25:19 +0530

    store user data in user context

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,12 +1,69 @@
+import gql from 'graphql-tag';
 import Header from './Header';
 import Nav from './Nav';
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import UserContext from '../context/UserContext';
+import { IApolloContext } from '../interfaces/apollo';
+import { useApolloContext } from '../hooks/apolloClient';
 
 export default function Dashboard() {
+  const { client }: IApolloContext = useApolloContext();
+  const [userData, setUserData] = useState({ loading: true });
+
+  useEffect(() => {
+    if (client !== undefined) {
+      client
+        .query({
+          query: gql`
+            {
+              user(login: "amanshu-kataria") {
+                bio
+                createdAt
+                email
+                followers(first: 20) {
+                  totalCount
+                }
+                following {
+                  totalCount
+                }
+                repositories {
+                  totalCount
+                }
+                starredRepositories {
+                  totalCount
+                }
+              }
+            }
+          `
+        })
+        .then((response: any) => {
+          const { user } = response.data;
+          const userData = {
+            followers: user.followers.totalCount,
+            following: user.following.totalCount,
+            repositories: user.repositories.totalCount,
+            starredRepositories: user.starredRepositories.totalCount,
+            loading: false
+          };
+          setUserData(userData);
+        })
+        .catch((err: any) => {
+          console.log(err);
+        });
+    }
+  });
   return (
     <div className="gg__dashboard">
       <Header />
-      <Nav />
+      <UserContext.Provider value={userData}>
+        {userData.loading ? (
+          'Loading ...'
+        ) : (
+          <Fragment>
+            <Nav />
+          </Fragment>
+        )}
+      </UserContext.Provider>
     </div>
   );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,31 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
+import UserContext from '../context/UserContext';
 import { Label, Menu } from 'semantic-ui-react';
 
-const menuItems = ['Repositories', 'Stars', 'Followers', 'Following'];
+const menuItems = [
+  { label: 'Repositories', prop: 'repositories' },
+  { label: 'Stars', prop: 'starredRepositories' },
+  { label: 'Followers', prop: 'followers' },
+  { label: 'Following', prop: 'following' }
+];
 
 function Nav() {
   const [activeItem, setActiveItem] = useState(0);
   return (
-    <Menu vertical className="gg__nav">
-      {menuItems.map((item, index) => {
-        return (
-          <Menu.Item key={index} name={item} active={activeItem === index} onClick={() => setActiveItem(index)}>
-            <Label color={activeItem === index ? 'teal' : 'grey'}>1</Label>
-            {item}
-          </Menu.Item>
-        );
-      })}
-    </Menu>
+    <UserContext.Consumer>
+      {(user: any) => (
+        <Menu vertical className="gg__nav">
+          {menuItems.map((item, index) => {
+            return (
+              <Menu.Item key={index} name={item.label} active={activeItem === index} onClick={() => setActiveItem(index)}>
+                <Label color={activeItem === index ? 'teal' : 'grey'}>{user.loading ? '' : user[item.prop]}</Label>
+                {item.label}
+              </Menu.Item>
+            );
+          })}
+        </Menu>
+      )}
+    </UserContext.Consumer>
   );
 }
 
