533ae6715266ce0833250d22714168d8363587e2
Amanshu Kataria <amanshu.kataria@elucidata.io>
2020-06-20 17:29:37 +0530

    added query fragments for user query

---
+++ src/queries/user.ts
@@ -0,0 +1,88 @@
+import gql from 'graphql-tag';
+
+export function userProfile() {
+  return gql`
+    query($userName: String!) {
+      user(login: $userName) {
+        bio
+        createdAt
+        email
+        followers(first: 20, after: null) {
+          ...Followers
+        }
+        following(first: 20, after: null) {
+          ...Following
+        }
+        repositories(first: 20, after: null) {
+          ...Repositories
+        }
+        starredRepositories(first: 20, after: null) {
+          ...StarredRepositories
+        }
+      }
+    }
+    ${user.fragments.starredRepositories}
+    ${user.fragments.repositories}
+    ${user.fragments.followers}
+    ${user.fragments.following}
+  `;
+}
+
+export const user = {
+  fragments: {
+    repositories: gql`
+      fragment Repositories on RepositoryConnection {
+        edges {
+          node {
+            description
+            name
+            url
+          }
+          cursor
+        }
+        totalCount
+      }
+    `,
+    starredRepositories: gql`
+      fragment StarredRepositories on StarredRepositoryConnection {
+        edges {
+          node {
+            description
+            name
+            url
+          }
+          cursor
+        }
+        totalCount
+      }
+    `,
+    followers: gql`
+      fragment Followers on FollowerConnection {
+        edges {
+          cursor
+          node {
+            avatarUrl(size: 10)
+            bio
+            name
+            url
+          }
+        }
+        totalCount
+      }
+    `,
+    following: gql`
+      fragment Following on FollowingConnection {
+        edges {
+          cursor
+          node {
+            avatarUrl(size: 10)
+            bio
+            name
+            url
+          }
+        }
+        totalCount
+      }
+    `
+  }
+};
