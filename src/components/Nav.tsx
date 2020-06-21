eff30a9437e8f59b07b9d3059390a7f074628805
Amanshu Kataria <amanshukataria@hotmail.com>
2020-06-21 23:30:33 +0530

    Nav bar updated

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import UserContext from '../context/UserContext';
 import { Label, Menu } from 'semantic-ui-react';
+import { NavProps } from '../interfaces/UI';
 
 const menuItems = [
   { label: 'Repositories', prop: 'repositories' },
@@ -9,16 +10,22 @@ const menuItems = [
   { label: 'Following', prop: 'following' }
 ];
 
-function Nav() {
+function Nav({ onNavItemChange }: NavProps) {
   const [activeItem, setActiveItem] = useState(0);
+
+  function onNavItemClick(index: number, prop: string) {
+    setActiveItem(index);
+    onNavItemChange(prop);
+  }
+
   return (
     <UserContext.Consumer>
       {(user: any) => (
         <Menu vertical className="gg__nav">
           {menuItems.map((item, index) => {
             return (
-              <Menu.Item key={index} name={item.label} active={activeItem === index} onClick={() => setActiveItem(index)}>
-                <Label color={activeItem === index ? 'teal' : 'grey'}>{user.loading ? '' : user[item.prop]}</Label>
+              <Menu.Item key={index} name={item.label} active={activeItem === index} onClick={() => onNavItemClick(index, item.prop)}>
+                <Label color={activeItem === index ? 'teal' : 'grey'}>{user.loading ? '' : user[item.prop].totalCount}</Label>
                 {item.label}
               </Menu.Item>
             );
